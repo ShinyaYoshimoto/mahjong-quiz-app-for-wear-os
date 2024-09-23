75f9daaeac7b09dee535738eb9961757f64d4a7f
ysmt <shinya.yoshimoto.67@gmail.com>
2024-09-23 11:34:38 +0900

    愚直にクイズを出せるようにする

--- app/src/main/java/com/example/mahjongquizapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mahjongquizapp/presentation/MainActivity.kt
@@ -9,23 +9,28 @@ package com.example.mahjongquizapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.ListHeader
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
-import com.example.mahjongquizapp.R
 import com.example.mahjongquizapp.presentation.theme.MahjongQuizAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +41,13 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp()
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp() {
     MahjongQuizAppTheme {
         Box(
             modifier = Modifier
@@ -50,24 +55,49 @@ fun WearApp(greetingName: String) {
                 .background(MaterialTheme.colors.background),
             contentAlignment = Alignment.Center
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                TimeText()
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                ScalingLazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    item { ListHeader { Quiz() } }
+                    items(20) {
+                        Chip(
+                            onClick = {},
+                            label = { Text("List item $it") },
+                            colors = ChipDefaults.secondaryChipColors()
+                        )
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(greetingName: String) {
+fun Quiz() {
+    // とりあえず愚直に書いていく
+    val isParent = (0..1).random()
+    val symbolCount = (2..11).random() * 10
+    val fanCount = (1..13).random()
+    val isRon = (0..1).random()
     Text(
         modifier = Modifier.fillMaxWidth(),
         textAlign = TextAlign.Center,
         color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
+        text = "${if (isParent == 1) "親" else "子"} ${symbolCount}符 ${fanCount}翻 ${if (isRon == 1) "ロン" else "ツモ"}"
     )
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
